47dfdff25dcc9492b40906d69725f608e9f1ec29
Lauryn Langford <laurynashlangford@gmail.com>
2019-07-23 10:34:54 -0500

    deleted ds.store file

--- ToDoList/Assets/js/app.js
+++
@@ -1,9 +0,0 @@
-function addLi(){
-	var txtVal = document.getElementById('txtVal').value,
-		listNode = document.getElementById('list'),
-		liNode = document.createElement('LI'),
-		txtNode = document.createTextNode(txtVal);
-
-	liNode.appendChild(txtNode);
-	listNode.appendChild(liNode);
-}
