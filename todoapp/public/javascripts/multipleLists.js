5e9a9ed8f01d7ce7907d27e794bf8d3501ee9b3c
hernanespinal <Haespina@my.loyno.edu>
2019-07-26 10:05:22 -0700

    Geoff Changes

--- todoapp/public/javascripts/multipleLists.js
+++ todoapp/public/javascripts/multipleLists.js
@@ -16,12 +16,15 @@
 	var list = document.getElementById("savedLists");
 	var add = document.getElementById('btnSave');
 
+	var lists = [];
+
 	var setTitle = function(){
 	listTitle.innerText=userInput.value;
 	};
 
 	var alert = function(){
 				var box = alertBox.innerHTML += '<div class="alert alert-warning" role="alert"> Please remember to save your list! would you like to continue?' + '<button>No</button><button>Yes</button>' + '<br> This message will disappear in 8 seconds. </div>'
+	
 	};
 
 	var refresh = function(){
@@ -33,7 +36,9 @@
 
 	add.addEventListener('click', function(){
 		var itemsByTagName = document.getElementsByTagName("li");
-		list.innerHTML += '<li><div class="card"><div class="card-body"><h5 class="card-title">'+ listTitle.innerText +'</h5><br><a href="../multipleLists" class="btn btn-primary">Check out my list</a></div></div> </li>'
+		var cards = document.querySelectorAll('.card');
+
+		list.innerHTML += '<li><div class="card"><div class="card-body"><h5 class="card-title">'+ listTitle.innerText +'</h5><br><a href="../multipleLists?id=' + cards.length + '" class="btn btn-primary">Check out my list</a></div></div> </li>'
 	});
 
 // var setCardTitle = function(){
